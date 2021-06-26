edf84722a8acdb113ddba6cff49d09ad9cc0c526
Nathan-Hug <74884291+Nathan-Hug@users.noreply.github.com>
2021-06-25 20:44:52 -0700

    Notes App
    Javascript file with working DOM manipulation

--- crud.js
+++ crud.js
@@ -1,58 +1,32 @@
 function onFormSubmit() {
-	var formData = readFormData();
-	insertNewRecord(formData);
+	var formData = readFormData(); //get data from from
+	insertNewRecord(formData); //adding data from form to table
 	resetForm();
 }
 
 function readFormData() {
 	var formData = {};
-	formData["fullName"] = document.getElementById("fullName").value;
-	formData["empCode"] = document.getElementById("empCode").value;
-	formData["salary"] = document.getElementById("salary").value;
-	formData["city"] = document.getElementById("city").value;
+	formData["notes"] = document.getElementById("notes").value;
 	return formData;
 }
 
+//adding data to rows in table
 function insertNewRecord(data) {
-	var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+	var table = document.getElementById("completeNotes").getElementsByTagName('tbody')[0];
 	var newRow = table.insertRow(table.length);
 	cell1 = newRow.insertCell(0);
-	cell1.innerHTML = data.fullName;
-	cell2 = newRow.insertCell(1);
-	cell2.innerHTML = data.empCode;
-	cell3 = newRow.insertCell(2);
-	cell3.innerHTML = data.salary;
-	cell4 = newRow.insertCell(3);
-	cell4.innerHTML = data.city;
-	cell5 = newRow.insertCell(4);
-	cell5.innerHTML = `<a onClick="onEdit(this)">Edit</a><a onClick="onDelete(this)">Delete</a>`;
+	cell1.innerHTML = data.notes;
 }
 
-function resetForm() {
-	document.getElementById("fullName").value = "";
-	document.getElementById("empCode").value = "";
-	document.getElementById("salary").value = "";
-	document.getElementById("city").value = "";	
+// Reset Form
+function resetForm () {
+	document.getElementById("notes").value="";
 }
 
-function onEdit(td) {
-	selectedRow = td.parentElement.parentElement;
-	document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-	document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
-	document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
-	document.getElementById("city").value = selectedRow.cells[3].innerHTML;
-}
-
-function updateRecord(formData) {
-	selectedRow.cells[0].innerHTML = formData.fullName;
-	selectedRow.cells[1].innerHTML = formData.empCode;
-	selectedRow.cells[2].innerHTML = formData.salary;
-	selectedRow.cells[3].innerHTML = formData.city;
-}
-
-function onDelete(td) {
-	if(confirm('Are you sure you want to delete this record?')) {
-		row = td.parentElement.parentElement;
-		document.getElementById("employeeList").deleteRow(row.rowIndex);
-	}
+deleteNoteRow = function () {
+	//row = tr
+	//document.getElementById("completeNotes").deleteRow(row.rowIndex);
+	//console.log(row);
+	var td = event.target.parentElement; 
+	td.remove();
 }
