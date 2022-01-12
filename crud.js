d278505394323c8bee4fec2a615bc95b71c06cae
Nathan Hug <njh891@gmail.com>
2022-01-11 17:40:14 -0800

    crud app updated

--- crud.js
+++ crud.js
@@ -1,6 +1,13 @@
+
+var selectedRow = null;
+
 function onFormSubmit() {
-	var formData = readFormData(); //get data from from
-	insertNewRecord(formData); //adding data from form to table
+	var formData = readFormData(); //get data from form
+	if (selectedRow == null) 
+		insertNewRecord(formData);
+	else 
+		updateRecord(formData);
+	//insertNewRecord(formData); //adding data from form to table
 	resetForm();
 }
 
@@ -15,15 +22,33 @@ function insertNewRecord(data) {
 	var table = document.getElementById("completeNotes").getElementsByTagName('tbody')[0];
 	var newRow = table.insertRow(table.length);
 	cell1 = newRow.insertCell(0);
+	cell1.id = "notesRow";
 	cell1.innerHTML = data.notes;
+	cell2 = newRow.insertCell(1);
+	cell2.innerHTML = '<a id="editNoteButton" class="button" onclick="editNotes(this)">Edit</a>';
+	cell3 = newRow.insertCell(2);
+	cell3.innerHTML = '<a id="deleteNoteButton" class="button" onclick="deleteNotes(this)">Delete</a>';
 }
+
+function updateRecord(formData) {
+	selectedRow.cells[0].innerHTML = formData.notes;
+}
+
+function editNotes(td) {
+	selectedRow = td.parentElement.parentElement;
+	document.getElementById("notes").value = selectedRow.cells[0].innerHTML;
+	document.getElementById("notes").focus();
+}
+
 
 // Reset Form
 function resetForm () {
 	document.getElementById("notes").value="";
+	document.getElementById("notes").focus();
+	selectedRow = null;
 }
 
-deleteNoteRow = function () {
-	var td = event.target.parentElement; 
-	td.remove();
+deleteNotes = function () {	
+	var tr = event.target.parentElement.parentElement; 
+	tr.remove();
 }
